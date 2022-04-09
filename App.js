bc72a08ac2cafb94d1db8004bf56fd094f21ab1a
higorj <higor_sgomes@outlook.com>
2022-04-09 15:38:16 -0300

    Desenvolvimento de um gerador de números

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [numeros, setNumeros] = useState([]);
+
+  function gerarNumeros() {
+    let numerosGerados = [];
+
+    for(let i = 0; i < 6; i++) {
+      let numeroGerado = Math.floor(Math.random() * 60 + 1);
+
+      while(numerosGerados.indexOf(numeroGerado) != -1) {
+        numeroGerado = Math.floor(Math.random() * 60 + 1)
+      }
+
+      numerosGerados.push(numeroGerado);
+    }
+
+    setNumeros(numerosGerados);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Gerador de Números</Text>
+      <Text>{ numeros.map(numero => numero + " ") }</Text>
+
+      <Button 
+        title='Gerar números' 
+        onPress={() => gerarNumeros()} 
+      />
     </View>
   );
 }
